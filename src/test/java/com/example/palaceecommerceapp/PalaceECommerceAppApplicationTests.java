1cc5418f188c1e01a794201efc63659d2fa5fbab
Adio3 <a.awwal@native.semicolon.africa>
2024-02-22 17:48:23 +0100

    first commit

---
+++ src/test/java/com/example/palaceecommerceapp/PalaceECommerceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.palaceecommerceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PalaceECommerceAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
